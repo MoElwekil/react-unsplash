9c675ff429b6820009a2c9fffdbdd24dea505b0c
Mohamed Elwekil <Mo@Descod.net>
2021-02-22 11:43:50 +0000

    auto refresh issue fixed

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+// BOOTSTRAP
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
