a095f7e914dc25f0995e0d0b055738cda9573e5c
Mohamed Elwekil <Mo@Descod.net>
2021-02-22 11:47:57 +0000

    unsplash axios api setup complete

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,2 +1,11 @@
 // import axios
 import axios from 'axios'
+
+export default axios.create(
+    {
+        baseURL: 'https://api.unsplash.com',
+        headers: {
+            Authorization: Client-ID YOUR_ACCESS_KEY
+        }
+    }
+)
